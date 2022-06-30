5b266b401a4fbd7312f2c25ea404b772393cfcea
Fabricio-odn <fabriciodeoliveira334@gmail.com>
2022-06-30 16:22:05 -0300

    Parametro URL

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll('.header-menu a');
-
 function ativarLink(link) {
   const url = location.href;
   const href = link.href;
@@ -7,5 +7,16 @@ function ativarLink(link) {
     link.classList.add('ativo');
   }
 }
-
 links.forEach(ativarLink);
+
+// Ativar items do orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto (parametro) {
+  const elemento = document.getElementById(parametro);
+  if(elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
